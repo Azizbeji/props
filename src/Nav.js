e6569002e3a800d85b70acae73a74ca1cf0ef969
Azizbeji <azizbeji50@gmail.com>
2023-12-12 11:44:24 +0100

    my checkpoint prject about react react props chekpoint

---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <div className="nav" >
+        <img className="img"
+          src="https://admin.itsnicethat.com/images/hs-liHgeD3soOAeI5jOF9rRJ_Tw=/239634/format-webp%7Cwidth-2880/uncommon-creative-studio-ea-sports-fc-graphic-design-itsnicethat-01.jpg"
+          alt=""
+        />
+        <div className="ea">EA FC24</div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav
